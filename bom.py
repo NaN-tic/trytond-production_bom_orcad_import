358a52bb15151318ba4d2898ec38c8cbe91886b5
Raimon Esteve <raimon@nan-tic.com>
2018-02-02 14:59:47 +0100

    FIX version number

--- bom.py
+++ bom.py
@@ -1,6 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.wizard import Wizard, StateView, Button, StateAction
 from trytond.transaction import Transaction
